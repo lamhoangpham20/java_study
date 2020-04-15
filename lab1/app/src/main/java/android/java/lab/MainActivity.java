28467bd6e6f6c6972f9d3083c883c36163640d51
Lam Pham <t8phho00@students.oamk.fi>
2020-04-15 13:41:42 +0300

    Debug lab 1

--- lab1/app/src/main/java/android/java/lab/MainActivity.java
+++ lab1/app/src/main/java/android/java/lab/MainActivity.java
@@ -20,60 +20,24 @@ import android.widget.TextView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final int REQ_CODE= 12345;
+public class MainActivity extends AppCompatActivity {
+    private static final int REQ_CODE = 12345;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*LinearLayout parent= new LinearLayout(this);
-        parent.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT ,ViewGroup.LayoutParams);
-        parent.setOrientation(LinearLayout.VERTICAL);
-        Button button = new Button(this);
-        button.setText("Press Me!");
-        button.setOnClickListener(this);
-        parent.addView(button);
-        TextView textView=new TextView(this);
-        textView.setText("Hello");
-        parent.addView(textView);*/
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.layout, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.settings)
-        {
-            //
-        }
-        return true;
-    }
-
-    @Override
-    public void onClick(View v) {
-        if(v.getId() == R.id.openActivity)
-        {
-            EditText editor = findViewById(R.id.textEditor);
-            String tel = editor.getText().toString();
-
-            Intent intent = new Intent(this, PhoneDetailActivity.class);
-            intent.putExtra("PHONE_NUMBER", tel);
-
-            startActivityForResult(intent, REQ_CODE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQ_CODE && resultCode == RESULT_OK)
-        {
-
-        }
+        final String[] COUNTRIES = new String[]{
+                "Afghanistan","Albania", "Angeria", "American Samoa", "Andorra",
+                "Angola","Anguilla","Antarctica", "Antigua and Barbuda", "Argentina",
+                "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan"
+        };
+        ListView myListView = (ListView) findViewById(R.id.countries_list_view);
+        final ArrayAdapter<String> aa;
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, COUNTRIES);
+        myListView.setAdapter(aa);
     }
 }
+
+
+
